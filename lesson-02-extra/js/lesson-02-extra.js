d45cdb9dfeb62409fac4690a33157dcf28ca7a40
Ширманов Дмитрий Александрович <d.shirmanov@bft.local>
2018-08-10 14:26:43 +0700

    Доделал второе дополнительное задание

--- lesson-02-extra/js/lesson-02-extra.js
+++ lesson-02-extra/js/lesson-02-extra.js
@@ -20,3 +20,38 @@ function getDayOfWeek(arr){
 }
 
 getDayOfWeek(week);
+
+//---Второе дополнительное задание---//
+
+let arr = [];
+let countItem=7;
+let i=1;
+
+function checkNumber(x) {
+  if (x.length>1) {
+   x++;	  
+   if (typeof(x) == 'number') {
+     return true;
+   };
+  }
+  return false;  
+}
+	
+while (i <= countItem) {
+  let num = prompt("Введите "+i+" многозначное число","" );
+  checkNumber(num)?arr.push(num):alert("Число задано не верно!");  
+  i++;  
+}	
+
+function getItemArr(arr){
+  arr.forEach(function(element, index) {	  
+                switch (element.substring(0,1)) {
+                  case '3':
+                  case '7':
+                    document.write("<p><strong>"+element+"</strong></p>");
+                    break;
+                }                
+              });
+}
+
+getItemArr(arr);
