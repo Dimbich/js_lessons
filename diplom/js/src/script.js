7a0c2a1d4b17218759de75ca110358c84b913119
Dmitriy <shirmanovdmitriy@gmail.com>
2018-09-09 21:56:35 +0700

    Добавил отображение модального окна подарка

--- diplom/js/src/script.js
+++ diplom/js/src/script.js
@@ -3,9 +3,11 @@
   let showexamplework = require('../parts/showexamplework.js');
   let portfolio = require('../parts/portfolio.js');
   let calc = require('../parts/calc.js');
+  let modalGift = require('../parts/modalGift.js');
 
   picture();
   showexamplework();
   portfolio();
   calc();
+  modalGift();
   
